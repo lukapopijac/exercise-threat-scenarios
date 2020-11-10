68e0a2b71d1ea916b51b810b1da085d550349f92
Luka Popijac <luka.popijac@gmail.com>
2020-11-10 15:08:38 -0800

    style table

--- frontend/src/components/ThreatScenariosTableRow/ThreatScenariosTableRow.js
+++ frontend/src/components/ThreatScenariosTableRow/ThreatScenariosTableRow.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './ThreatScenariosTableRow.css';
 import AddUpdateThreatScenarioForm from '../AddUpdateThreatScenarioForm/AddUpdateThreatScenarioForm.js';
 
+const riskLevelText = {
+	0: 'Information',
+	1: 'Low risk',
+	2: 'Med risk',
+	3: 'High risk'
+};
+
 export default class ThreatScenariosTableRow extends React.Component {
 	constructor(props) {
 		super(props);
@@ -17,7 +24,7 @@ export default class ThreatScenariosTableRow extends React.Component {
 				<div className="classification">{ts.classification}</div>
 				<div className="impact"        >{ts.impact}</div>
 				<div className="likelihood"    >{ts.likelihood}</div>
-				<div className="risk_level"    >{ts.risk_level}</div>
+				<div className="risk_level"    >{riskLevelText[ts.risk_level]} ({ts.risk_level})</div>
 				<div className="actions">
 					<button onClick={this.props.onEditClick} className="btn-edit"><i className="material-icons">edit</i></button>
 					<button onClick={this.props.onDeleteClick} className="btn-delete"><i className="material-icons">delete</i></button>
